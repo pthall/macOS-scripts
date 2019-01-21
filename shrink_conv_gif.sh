ce259482789800ce5dd7190b5a230569f550fcf3
pthall <pt.hall@gmail.com>
2019-01-21 15:47:09 -0800

    Update shrink_conv_gif.sh

--- shrink_conv_gif.sh
+++ shrink_conv_gif.sh
@@ -1,3 +1,2 @@
 #/bin/bash
-DIV=1
-sips -s format gif --resampleHeight $(($(sips -g pixelHeight "$(echo $1)" | awk '{print $2}')/${2:-DIV})) $(echo $1) --out $(echo $(echo $1 | sed -E 's/(.*)\.[a-zA-Z0-9]+/\1/')".gif")
+sips -s format gif --resampleHeight $(($(sips -g pixelHeight "${1}" | grep pixelHeight | awk '{print $2}')/${2})) "${1}" --out "${1%.*}.gif"
