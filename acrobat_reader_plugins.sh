5808a86b0da1fa351926ada1c5b982f51472600d
Peter Hall <peter.hall@sage.com>
2021-02-22 11:38:17 -0800

    fixes

--- acrobat_reader_plugins.sh
+++ acrobat_reader_plugins.sh
@@ -4,13 +4,16 @@ DISABLE_DIRS=( Accessibility.acroplugin Comments.acroplugin MakeAccessible.acrop
 export PLUGINS_DIR="/Applications/Adobe Acrobat Reader 2020.app/Contents/Plugins"
 export DISABLED_PLUGINS_DIR="/Applications/Adobe Acrobat Reader 2020.app/Contents/Disabled Plugins"
 
+set -x
+
 case "$1" in
   disable-plugins)
-    set -x
     [[ ! -e "$DISABLED_PLUGINS_DIR" ]] && sudo mkdir "$DISABLED_PLUGINS_DIR"
 
+    # must delete any dirs already in DISABLED_PLUGINS_DIR before moving PLUGINS_DIR dirs
     for dir in "${DISABLE_DIRS[@]}" ; do
-      [[ -d "$PLUGINS_DIR/$dir" ]] && sudo mv -f "$PLUGINS_DIR/$dir" "$DISABLED_PLUGINS_DIR/"
+      [[ -d "$DISABLED_PLUGINS_DIR/$dir" ]] && [[ -d "$PLUGINS_DIR/$dir" ]] && rm -rf "$DISABLED_PLUGINS_DIR/$dir"
+      [[ -d "$PLUGINS_DIR/$dir" ]] && sudo mv "$PLUGINS_DIR/$dir" "$DISABLED_PLUGINS_DIR/"
     done
     ;;
 
@@ -18,11 +21,11 @@ case "$1" in
     # move disabled plugins not already in the plugins dir;
     # remove disabled plugins also in the plugins dir
     find -d "$DISABLED_PLUGINS_DIR" -depth 1 -exec sh -c \
-      'set -x; \
-      if [[ ! -e "$PLUGINS_DIR/$(basename "{}")" ]]; then \
+      'if [[ ! -e "$PLUGINS_DIR/$(basename "{}")" ]]; then \
         sudo mv "{}" "$PLUGINS_DIR/"; \
       else
-        sudo rm -r "{}"; \
+        sudo rm -rf "{}"; \
       fi' \;
     ;;
 esac
+
